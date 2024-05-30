ba72f0fd8a81961aa56133a26fc5bea06b2bb4b4
Felix Ojiambo <felixojiamboe@gmail.com>
2024-05-30 06:06:30 -0700

    data service

--- src/app/features/dashboard/overview/overview.component.ts
+++ src/app/features/dashboard/overview/overview.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { TopCardComponent } from "../../../shared/components/top-card/top-card.component";
 
 @Component({
-  selector: 'app-overview',
-  standalone: true,
-  imports: [],
-  templateUrl: './overview.component.html',
-  styleUrl: './overview.component.css'
+    selector: 'app-overview',
+    standalone: true,
+    templateUrl: './overview.component.html',
+    styleUrl: './overview.component.css',
+    imports: [TopCardComponent]
 })
 export class OverviewComponent {
-
+  collectionsCount = 120; 
+  signupsCount = 30; 
+  totalRevenue = 15000; 
+  bouncedChequesCount = 2; 
 }
